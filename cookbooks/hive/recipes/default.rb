31f648db45383a34dd9efff97ac4eb21e3dacf19
Herminio Vazquez <canimus@gmail.com>
2016-10-26 14:25:51 +0200

    Addition to mysql recipe

--- cookbooks/hive/recipes/default.rb
+++ cookbooks/hive/recipes/default.rb
@@ -22,3 +22,25 @@ execute 'extract_hive_tar' do
   action :nothing
   not_if { Dir.exist? "/vagrant/apache-hive-2.1.0-bin/" }
 end
+
+# template '/vagrant/apache-hive-2.1.0-bin/conf/hive-site.xml' do
+#   source 'hive-site.xml.erb'
+#   owner 'vagrant'
+#   group 'vagrant'
+#   mode '0755'
+#   variables :hdfs => {"server" => "hdfs://10.10.10.20", "port" => "10020"}
+# end
+
+cookbook_file "/vagrant/apache-hive-2.1.0-bin/conf/hive-env.sh" do
+  source 'hive-env.sh'
+  owner 'vagrant'
+  group 'vagrant'
+  mode '0755'
+end
+
+mysql_service 'mysql' do
+  port '3306'
+  version '5.6'
+  initial_root_password 'w3lcom3'
+  action [:create, :start]
+end
