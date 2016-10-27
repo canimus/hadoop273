0b3e86d0e1e3a7dba0409319a04e00e3879e2f01
Herminio Vazquez <Herminio Vazquez>
2016-10-27 23:43:10 +0200

    Working HIVE deployment with minimum configuration

--- cookbooks/hive/recipes/default.rb
+++ cookbooks/hive/recipes/default.rb
@@ -5,49 +5,43 @@
 # Copyright 2016, Iovio Big Data
 # ================================
 
-remote_file "/vagrant/apache-hive-2.1.0-bin.tar.gz" do
-  source 'http://apache.rediris.es/hive/hive-2.1.0/apache-hive-2.1.0-bin.tar.gz'
+remote_file "/vagrant/apache-hive-1.2.1-bin.tar.gz" do
+  source 'http://apache.rediris.es/hive/hive-1.2.1/apache-hive-1.2.1-bin.tar.gz'
   owner 'vagrant'
   group 'vagrant'
   mode '0755'
   action :create
   notifies :run, 'execute[extract_hive_tar]', :immediately
-  not_if { File.exist? "/vagrant/apache-hive-2.1.0-bin.tar.gz"}
+  not_if { File.exist? "/vagrant/apache-hive-1.2.1-bin.tar.gz"}
 end
 
 execute 'extract_hive_tar' do
-  command 'tar -xvf apache-hive-2.1.0-bin.tar.gz'
+  command 'tar -xvf apache-hive-1.2.1-bin.tar.gz'
   cwd '/vagrant'
   user 'vagrant'
   action :nothing
-  not_if { Dir.exist? "/vagrant/apache-hive-2.1.0-bin/" }
+  not_if { Dir.exist? "/vagrant/apache-hive-1.2.1-bin/" }
 end
 
-template '/vagrant/apache-hive-2.1.0-bin/conf/hive-site.xml' do
+template '/vagrant/apache-hive-1.2.1-bin/conf/hive-site.xml' do
   source 'hive-site.xml.erb'
   owner 'vagrant'
   group 'vagrant'
   mode '0755'
   variables :hive => {
-    "metastoredb" => "jdbc:mysql://0.0.0.0.6:3306/metastoredb?createDatabase IfNotExist=true",
-    "driver" => "com.mysql.jdbc.Driver",
-    "dbuser" => "hive",
-    "dbpassword" => "hive"
+    "dbmetastore" => "jdbc:mysql://localhost:3306/metastoredb",
+    "dbdriver" => "com.mysql.jdbc.Driver",
+    "dbuser" => "root",
+    "dbpassword" => "root"
   }
 end
 
-# cookbook_file "/vagrant/apache-hive-2.1.0-bin/conf/hive-env.sh" do
-#   source 'hive-env.sh'
-#   owner 'vagrant'
-#   group 'vagrant'
-#   mode '0755'
-# end
-
-cookbook_file "/vagrant/apache-hive-2.1.0-bin/lib/mysql-connector-java-5.1.40-bin.jar" do
+cookbook_file "/vagrant/apache-hive-1.2.1-bin/lib/mysql-connector-java-5.1.40-bin.jar" do
   source 'mysql-connector-java-5.1.40-bin.jar'
   owner 'vagrant'
   group 'vagrant'
   mode '0755'
+  not_if { Dir.exist? "/vagrant/apache-hive-1.2.1-bin/lib/mysql-connector-java-5.1.40-bin.jar" }
 end
 
 execute 'update_sources' do
@@ -82,5 +76,23 @@ end
 execute "tmp_permissions" do
   command "bin/hdfs dfs -chmod g+w /tmp"
   cwd "/vagrant/hadoop-2.7.3"
+  user "vagrant"
+end
+
+execute "password_mysql" do
+  command "mysqladmin -u root -h localhost password root"
+  cwd "/vagrant/apache-hive-1.2.1-bin"
+  user "vagrant"
+end
+
+execute "create_metastore_db" do
+  command "mysql -u root -proot -e \"create database metastoredb; GRANT ALL PRIVILEGES ON metastoredb.* TO root@localhost IDENTIFIED BY 'root'\""
+  cwd "/vagrant/apache-hive-1.2.1-bin"
+  user "vagrant"
+end
+
+execute "schema_init_hive" do
+  command "JAVA_HOME=/usr/lib/jvm/java-8-oracle/jre HADOOP_HOME=/vagrant/hadoop-2.7.3 HIVE_HOME=/vagrant/apache-hive-1.2.1-bin bin/schematool -dbType mysql -initSchema"
+  cwd "/vagrant/apache-hive-1.2.1-bin"
   user "vagrant"
 end
